fd2a50d60ad1eac464bc253f3d94f56cc5077deb
melsk-r <robert.melskens@vng.nl>
2023-09-13 13:33:41 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@ let respecConfig = {
   previousPublishDate: "2022-06-24",
   //previousMaturity: "WV",
   title: "SIM-Open Raads- en StatenInformatie",
-  content: {"Inleiding": ""},
+  content: {"Inleiding": "", "Scope": ""},
   editors:
     [
       {
