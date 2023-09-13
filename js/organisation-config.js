d8cc4c4c581318de452f00d5b5fb737315d721b9
melsk-r <robert.melskens@vng.nl>
2023-09-13 11:37:31 +0200

    Update organisation-config.js

--- js/organisation-config.js
+++ js/organisation-config.js
@@ -13,10 +13,10 @@ var organisationConfig = {
 
     postProcess: [window.respecMermaid.createFigures],
 
-    latestVersion: ["nl_organisationPublishURL", "pubDomain", "/", "shortName"],
+   // latestVersion: ["nl_organisationPublishURL", "pubDomain", "/", "shortName"],
    // thisVersion: ["nl_organisationPublishURL", "pubDomain", "/", "specStatus", "-", "specType", "-", "shortName", "-", "publishDate"],
     thisVersion: ["nl_organisationPublishURL", "pubDomain", "/", "shortName"],
-    prevVersion: ["nl_organisationPublishURL", "pubDomain", "/", "previousMaturity", "-", "specType", "-", "shortName", "-", "previousPublishDate"],
+   // prevVersion: ["nl_organisationPublishURL", "pubDomain", "/", "previousMaturity", "-", "specType", "-", "shortName", "-", "previousPublishDate"],
     useLogo: true,
     useLabel: true,
 
