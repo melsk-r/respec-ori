b1304a10bc98c5884096338d5a228a942bd290f1
melsk-r <robert.melskens@vng.nl>
2023-09-13 10:59:14 +0200

    Update organisation-config.js

--- js/organisation-config.js
+++ js/organisation-config.js
@@ -4,12 +4,12 @@ var organisationConfig = {
     nl_organisationStylesURL: "https://gitdocumentatie.logius.nl/publicatie/respec/style/",
     nl_organisationPublishURL: "https://gitdocumentatie.logius.nl/publicatie/",
     logos: [{
-        src: "https://gitdocumentatie.logius.nl/publicatie/respec/style/logos/figure-logius.svg",
-        alt: "Logius",
-        id: "Logius",
+        src: "https://melsk-r.github.io/Respec/logo-VNG-Realisatie.jpg",
+        alt: "VNG-Realisatie",
+        id: "VNG-Realisatie",
         height: 77,
         width: 44,
-        url: "https://www.logius.nl/standaarden",
+        url: "https://www.vng.nl",
     }],
 
     postProcess: [window.respecMermaid.createFigures],
