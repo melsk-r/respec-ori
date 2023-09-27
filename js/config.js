c1d32a98a47b354df9afb72fc16e2f4d2b27c43a
melsk-r <robert.melskens@vng.nl>
2023-09-27 14:45:29 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -21,7 +21,7 @@ let respecConfig = {
   //previousMaturity: "WV",
   title: "SIM-Open Raads- en StatenInformatie",
   content: {"SIM-ORI": ""},
-  content2: {"Inleiding": "pietje", "Scope": "puk", "SIM-ORI": ""},
+  content2: {"Inleiding": "informative", "Scope": "informative", "SIM-ORI": ""},
   // Hier wordt de variabele 'prevVersion' die gedefinieerd is in de 'organisation-config.js' overruled.
   // prevVersion: ["nl_organisationPublishURL", "previous", "/", "shortName"],
   
