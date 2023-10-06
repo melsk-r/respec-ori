5546cb4a3248a5f2fc7dc059774072a58d04c073
melsk-r <robert.melskens@vng.nl>
2023-10-06 17:03:27 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,17 +1,18 @@
 // Dit bestand wordt per standaard/document opnieuw van de juiste waarden voorzien.
 let respecConfig = {
-  otherLinks: [
-    {
-      key: "Referentie Implementaties",
-      data: [
-        {
-          value: "Open ORI",
-          href: "https://www.open-ORI.com",
-        },
-      ],
-    },
-  ],
-  subtitle: "The spec to end all specs.",
+//  otherLinks: [
+//    {
+//      key: "Referentie Implementaties",
+//      data: [
+//        {
+//          value: "Open ORI",
+//          href: "https://www.open-ORI.com",
+//        },
+//      ],
+//    },
+//  ],
+//  subtitle: "The spec to end all specs.",
+  testSuiteURI: "https://example.com/test/suite/",
 //  noTOC: true,
 //  maxTocLevel: 3,
 //  useLogo: true,
